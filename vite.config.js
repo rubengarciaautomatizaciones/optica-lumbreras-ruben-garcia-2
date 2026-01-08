4f2a8a227ba224ff32ccfcfec8e1e233fe6c5adf
rubengarciaautomatizaciones <ruben.automatizaciones@gmail.com>
2026-01-08 18:07:43 +0000

    Add shared alias

--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,14 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      // Aquí le decimos que la arroba @ apunta a client/src
-      "@": path.resolve(__dirname, "./client/src"),
+      // Atajo para la carpeta del cliente
+      "@": path.resolve(__dirname, "client", "src"),
+      // Atajo para la carpeta compartida (AQUÍ ESTABA EL ERROR)
+      "@shared": path.resolve(__dirname, "shared"),
     },
   },
   server: {
