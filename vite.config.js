98e6d8ea887ee3a563e413561bcfd0b85392da86
rubengarciaautomatizaciones <ruben.automatizaciones@gmail.com>
2026-01-08 18:05:37 +0000

    Fix alias config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// Configuración oficial para React + Vite
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      // Aquí le decimos que la arroba @ apunta a client/src
+      "@": path.resolve(__dirname, "./client/src"),
+    },
+  },
   server: {
     host: '0.0.0.0',
   },
   build: {
-    outDir: 'dist', // Obliga a guardar la web en la carpeta 'dist'
+    outDir: 'dist',
     emptyOutDir: true,
   }
 })
